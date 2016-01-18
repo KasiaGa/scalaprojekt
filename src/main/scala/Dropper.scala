d43bc1b98395eb372e2774f24d91519bd75d4d96
KasiaGa <kasik2121@gmail.com>
2016-01-18 19:18:52 +0100

    updated dropper

--- src/main/scala/Dropper.scala
+++ src/main/scala/Dropper.scala
@@ -12,12 +12,12 @@ class Dropper {
   val r = Random
   var y = 0
   val x = r.nextInt(951)
-  val i = ImageIO.read(new File("bomb.png"))
-  val rectangle = new Rectangle(x, y, 120, 60)
+  val i = ImageIO.read(new File("dropper.png"))
+  val rectangle = new Rectangle(x, y+60, 60, 60)
 
   def moveDown(): Unit = {
     if(y<=700) y += 5
-    rectangle.setBounds(x, y, 120, 60)
+    rectangle.setBounds(x, y+60, 60, 60)
   }
 
   def paint(g: Graphics2D, c: Canvas): Unit = {
